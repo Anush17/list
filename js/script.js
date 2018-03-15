35fb500326c128ddf977b4db6acb8e9dafde00c3
aaa17 <achakhoyan99@gmail.com>
2018-03-15 14:53:12 +0400

    added new  functions, printed user list when typed @ and  counted letters position

--- js/script.js
+++ js/script.js
@@ -1,27 +1,43 @@
 'use strict';
 
 $(function () {
-    function user_list() {
+    function userList() {
 
-        let names = ['Armen Gevorgyan', 'Gurgen Surenyan', 'Levon Levonyan', 'Armine Sahakyan', 'Hermine Gareginyan'];
-        let text = '';
+        let names = ['Armen Gevorgyan', 'Gurgen Surenyan', 'Levon Levonyan', 'Armine Sahakyan', 'Hermine Gareginyan'],
+            text = '',
+            txtArea = $('#txt'),
+            start = txtArea.prop('selectionStart'),
+            end = txtArea.prop('selectionStart');
+            $('#box').addClass('block');
 
-        $('#box').addClass('block');
-        $('#txt').on('keyup', function (e) {
-            e.preventDefault();
-
-            if (this.value.match(/[@]/)) {
-                for (let i = 0; i < names.length; i++) {
-                    text += names[i] + "<br>";
+        function eventBind() {
+            txtArea.on('click ', function (e) {
+                currentPosition();
+                e.preventDefault();
+            });
+            txtArea.on('keydown', function (e) {
+                if (e.keyCode === 37) {
+                    console.log(txtArea.prop('selectionStart'));
+                } else if (e.keyCode === 39) {
+                    console.log(txtArea.prop('selectionStart'));
                 }
-                $('#box').append(text).removeClass('block');
-            } else {
-                $('#box').addClass('block');
-                this.value = '';
+            });
+        }
+
+        function currentPosition() {
+            let txtAreaVal = $('#txt').val();
+            if (start >= 0 && start === end) {
+                if (txtAreaVal.match(/[@]/g)) {
+                    for (let i = 0; i < names.length; i++) {
+                        text += names[i] + "<br>";
+                    }
+                    $('#box').append(text).removeClass('block');
+                } else {
+                    $('#box').addClass('block');
+                }
             }
-        });
+        }
+        eventBind();
     }
-
-    user_list();
+    userList();
 });
-
