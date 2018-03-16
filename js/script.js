36501adaa2a7abb0b5bbcb8c78470cf7b7a31da2
aaa17 <achakhoyan99@gmail.com>
2018-03-16 14:49:49 +0400

    Changing some functionality and added new functions

--- js/script.js
+++ js/script.js
@@ -1,43 +1,69 @@
 'use strict';
 
 $(function () {
+
+    //Creating User List function, to see users when typing `@` sign
     function userList() {
 
-        let names = ['Armen Gevorgyan', 'Gurgen Surenyan', 'Levon Levonyan', 'Armine Sahakyan', 'Hermine Gareginyan'],
+        let names = ['Armen Gevorgyan', 'Gurgen Surenyan', 'Armine Sahakyan', 'Hermine Gareginyan'],
             text = '',
             txtArea = $('#txt'),
-            start = txtArea.prop('selectionStart'),
-            end = txtArea.prop('selectionStart');
-            $('#box').addClass('block');
+            list = $('#list'),
+            box = $('#box'),
+            start = txtArea.prop('selectionStart');
 
+        //Binding Events and getting left and right arrow positions
         function eventBind() {
-            txtArea.on('click ', function (e) {
-                currentPosition();
-                e.preventDefault();
-            });
+
             txtArea.on('keydown', function (e) {
-                if (e.keyCode === 37) {
+                let keyCode = e.keyCode;
+                if (keyCode === 37) {
                     console.log(txtArea.prop('selectionStart'));
-                } else if (e.keyCode === 39) {
+                } else if (keyCode === 39) {
                     console.log(txtArea.prop('selectionStart'));
+                } else {
+                    list.empty();
+                    //this function is for getting current position of cursor
+                    if (start >= 0) {
+                        checkSign(e);
+                    }
                 }
             });
         }
 
-        function currentPosition() {
-            let txtAreaVal = $('#txt').val();
-            if (start >= 0 && start === end) {
-                if (txtAreaVal.match(/[@]/g)) {
-                    for (let i = 0; i < names.length; i++) {
-                        text += names[i] + "<br>";
+        //filling data into list
+        function fillData() {
+            box.append(text).removeClass('disNone');
+            for (let i = 0; i < names.length; i++) {
+                list.append('<li id="' + names[i] + '">' + names[i] + '<br>' + '</li>');
+                $("#list li").click(function (e) {
+                    if (e.target && e.target.nodeName === "LI") {
+                        console.log(e.target.id + " was clicked");
+                        let attr = $(this).attr('id');
+                        txtArea.val(txtArea.val() + attr + ' ');
+                        box.addClass('disNone');
                     }
-                    $('#box').append(text).removeClass('block');
-                } else {
-                    $('#box').addClass('block');
-                }
+                });
+            }
+            list.each(function () {
+                $(this).children().slice(0, 2).hide();
+            });
+        }
+
+        //checking if have a `@` sign or not
+        function checkSign(eve) {
+            let charCode = String.fromCharCode(eve.keyCode);
+            let lastChar = txtArea.val().slice(-1);
+            console.log(charCode);
+            if (lastChar === '@') {
+                fillData();
+            } else {
+                box.addClass('disNone');
             }
         }
+
         eventBind();
     }
+
     userList();
 });
